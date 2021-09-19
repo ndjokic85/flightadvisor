045561baf1a53e2360a24a4d3c17f03ce3731471
Nikola <n.djokic@vegait.rs>
2021-09-19 10:29:37 +0200

    Autopopulate timestamp

--- application/app/Models/Role.php
+++ application/app/Models/Role.php
@@ -8,8 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Role extends Model
 {
     use HasFactory;
+
+    public $timestamps = true;
     
-    public function users() {
+    public function users()
+    {
         return $this->belongsToMany(User::class);
     }
 }
--- application/app/Models/User.php
+++ application/app/Models/User.php
@@ -44,8 +44,10 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    public function roles() {
+    public $timestamps = true;
+
+    public function roles()
+    {
         return  $this->belongsToMany(Role::class);
     }
-
 }
