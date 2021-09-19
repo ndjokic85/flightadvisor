21abcc2d1fb869a3af296ae25c3ec0335ba19f23
Nikola <n.djokic@vegait.rs>
2021-09-19 20:16:18 +0200

    Add api/v1 version to routes

--- application/routes/api.php
+++ application/routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::namespace('App\Http\Controllers\Api\v1')
+    ->prefix('v1')
+    ->group(function () {
+        Route::post('/users', 'AuthenticationController@create');
+    });
