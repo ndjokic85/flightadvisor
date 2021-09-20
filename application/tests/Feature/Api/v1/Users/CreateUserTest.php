e1de426ca69a27544f51270d9161e1ea1b248bf7
Nikola <n.djokic@vegait.rs>
2021-09-20 14:10:30 +0200

    Add new tests to CreateUserTest feature test class

--- application/tests/Feature/Api/v1/Users/CreateUserTest.php
+++ application/tests/Feature/Api/v1/Users/CreateUserTest.php
@@ -5,8 +5,7 @@ namespace Tests\Feature\Api\v1\Users;
 use App\Http\Resources\UserResource;
 use App\Models\Role;
 use App\Models\User;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Str;
 use Tests\TestCase;
 
 class CreateUserTest extends TestCase
@@ -30,5 +29,103 @@ class CreateUserTest extends TestCase
         $this->assertEquals($user->username, 'jon1985');
         $this->assertEquals($user->first_name, 'Jon');
         $this->assertTrue($user->hasRole($userRole->name));
+    }
+
+    /** @test */
+    public function username_is_required()
+    {
+        $response = $this->postJson('api/v1/users', [
+            'username' => ''
+        ]);
+        $response->assertStatus(422);
+        $response->assertJsonValidationErrors(['username']);
+        $this->assertSame(
+            'The username field is required.',
+            $response->json('errors.username.0')
+        );
+    }
+
+    /** @test */
+    public function first_name_is_required()
+    {
+        $response = $this->postJson('api/v1/users', [
+            'first_name' => ''
+        ]);
+        $response->assertStatus(422);
+        $response->assertJsonValidationErrors(['first_name']);
+        $this->assertSame(
+            'The first name field is required.',
+            $response->json('errors.first_name.0')
+        );
+    }
+
+    /** @test */
+    public function last_name_is_required()
+    {
+        $response = $this->postJson('api/v1/users', [
+            'last_name' => ''
+        ]);
+        $response->assertStatus(422);
+        $response->assertJsonValidationErrors(['last_name']);
+        $this->assertSame(
+            'The last name field is required.',
+            $response->json('errors.last_name.0')
+        );
+    }
+
+    /** @test */
+    public function password_is_required()
+    {
+        $response = $this->postJson('api/v1/users', [
+            'password' => ''
+        ]);
+        $response->assertStatus(422);
+        $response->assertJsonValidationErrors(['password']);
+        $this->assertSame(
+            'The password field is required.',
+            $response->json('errors.password.0')
+        );
+    }
+
+    /** @test */
+    public function password_must_be_at_least_6_characters()
+    {
+        $response = $this->postJson('api/v1/users', [
+            'password' => 'test'
+        ]);
+        $response->assertStatus(422);
+        $response->assertJsonValidationErrors(['password']);
+        $this->assertSame(
+            'The password must be at least 6 characters.',
+            $response->json('errors.password.0')
+        );
+    }
+
+    /** @test */
+    public function last_name_must_be_less_than_256_characters()
+    {
+        $response = $this->postJson('api/v1/users', [
+            'last_name' => Str::random(257),
+        ]);
+        $response->assertStatus(422);
+        $response->assertJsonValidationErrors(['last_name']);
+        $this->assertSame(
+            'The last name must not be greater than 255 characters.',
+            $response->json('errors.last_name.0')
+        );
+    }
+
+    /** @test */
+    public function first_name_must_be_less_than_256_characters()
+    {
+        $response = $this->postJson('api/v1/users', [
+            'first_name' => Str::random(257),
+        ]);
+        $response->assertStatus(422);
+        $response->assertJsonValidationErrors(['first_name']);
+        $this->assertSame(
+            'The first name must not be greater than 255 characters.',
+            $response->json('errors.first_name.0')
+        );
     }
 }
